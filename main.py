b495cda2bccf93d2fb7ed8369e4301aede257568
hjennings431 <45336734+hjennings431@users.noreply.github.com>
2021-03-10 15:46:31 +0000

    Update main.py
    Added reset fittest creature button, changes to graph scaling input

--- main.py
+++ main.py
@@ -20,18 +20,21 @@ def starthit():
     global last_plot1; global last_plot2; global last_plot3; global last_plot4; global last_plot5
     global gens_left
     global Population
+    global graph_points
     global count
 # For a restart reset everything you need here
     if not pause_sim:
-        draw_axis(pygame, Screen, Width, Height, BdrRight, BdrBottom)
         last_plot1 = (-1, -1)
         last_plot2 = (-1, -1)
         last_plot3 = (-1, -1)
         last_plot4 = (-1, -1)
         last_plot5 = (-1, -1)
-        Population = generate_creatures(NoOfBobs, XWorld, YWorld, Population, True, TurnsPerGen)
+        draw_axis(pygame, Screen, Width, Height, BdrRight, BdrBottom)
+        Population = generate_creatures(NoOfBobs, XWorld, YWorld, Population, True, TurnsPerGen, False)
         gens_left = Generations
         count = TurnsPerGen
+        graph_points = (Generations/10)
+        reset_fittest()
     pause_sim = False
 
 def stophit():
@@ -41,11 +44,12 @@ def stophit():
 
 def pausehit():
     global pause_sim; pause_sim=True
-
+def reset_fittest():
+    global fittest_creature; fittest_creature = Creature(0.7, 0.8, 0.8, 0.9, 59, 0.5, 0, (0, 0), False)
 # Variables for the organisms
 NoOfBobs = 100              # Number Of Creatures
 WldStop=False               # World Boundary - True=stop there, False=Wrap Around
-DisplaySpeed = 0           # Display update rate
+DisplaySpeed = 0            # Display update rate
 # Variables for the screen display
 Generations = 1000          # Define how many generations to run
 MovesPerTurn = 1            # Define how many moves to run per turn
@@ -85,7 +89,7 @@ DisplaySpeed_label = TextBox(Screen, 10, DisplaySpeed_WhereY, 100, 24, fontSize=
 DisplaySpeed_value = TextBox(Screen, 150, DisplaySpeed_WhereY, 50, 24, fontSize=24, colour=(background_color), textColour=slider_text_color, borderThickness=0)
 # Total Food Slider
 FoodPct_WhereY = 200
-FoodPct_slide = Slider(Screen, 10, FoodPct_WhereY+30, 190, 3, handleRadius=5, min=0, max=100, step=1, initial=FoodPct,  handleColour=(slider_handle_color), colour=(slider_color))
+FoodPct_slide = Slider(Screen, 10, FoodPct_WhereY+30, 190, 3, handleRadius=5, min=1, max=100, step=1, initial=FoodPct,  handleColour=(slider_handle_color), colour=(slider_color))
 FoodPct_label = TextBox(Screen, 10, FoodPct_WhereY, 100, 24, fontSize=24, colour=(background_color), textColour=slider_text_color, borderThickness=0)
 FoodPct_value = TextBox(Screen, 150, FoodPct_WhereY, 50, 24, fontSize=24, colour=(background_color), textColour=slider_text_color, borderThickness=0)
 # Tall Food Slider
@@ -109,13 +113,14 @@ draw_axis(pygame, Screen, Width, Height, BdrRight, BdrBottom)
 StartButton = Button(Screen, 10, Height-BdrBottom-20, 60, 20, text='Start', fontSize=24, inactiveColour=button_bg_color, pressedColour=button_hit_color, radius=4, onClick=lambda: starthit())
 StopButton = Button(Screen, 75, Height-BdrBottom-20, 60, 20, text='Stop', fontSize=24, inactiveColour=button_bg_color, pressedColour=button_hit_color, radius=4, onClick=lambda: stophit())
 PauseButton = Button(Screen, 140, Height-BdrBottom-20, 60, 20, text='Pause', fontSize=24, inactiveColour=button_bg_color, pressedColour=button_hit_color, radius=4, onClick=lambda: pausehit())
+ResetFittestButton = Button(Screen, 60, Height-BdrBottom-50, 100, 20, text='Reset Fittest', fontSize=24, inactiveColour=button_bg_color, pressedColour=button_hit_color, radius=4, onClick=lambda: reset_fittest())
 # Defining lists and 2d array for the world
 Population = []
 possible_x = []
 possible_y = []
 L_food = np.empty((XWorld,YWorld), dtype=object)
 # setting up a dummy fittest creature for the genetic algorithm to compare to
-fittest_creature =Creature(0.7, 0.8, 0.8, 0.9, 59, 0.5, 0, (0, 0), False)
+fittest_creature = Creature(0.7, 0.8, 0.8, 0.9, 59, 0.5, 0, (0, 0), False)
 # adding all possible x and y coords to 2 seperate lists
 for i in range(XWorld):
     possible_x.append(i)
@@ -130,12 +135,13 @@ all_coord_combos = [(a,b) for a in possible_x for b in possible_y]
 # initialising the first food and population
 reset_food(all_coord_combos, L_food)
 generate_food(all_coord_combos, FoodPct, TallFoodPct, BushFoodPct, L_food)
-Population = generate_creatures(NoOfBobs, XWorld, YWorld, Population, True, TurnsPerGen)
+Population = generate_creatures(NoOfBobs, XWorld, YWorld, Population, True, TurnsPerGen, False)
 
 
 # Just keep running until some event(s)
 count = TurnsPerGen
 gens_left = Generations
+graph_points = Generations/10
 while running:
     if (run_sim and not pause_sim):
         Population.sort(key=attrgetter('speed'), reverse=True)
@@ -155,23 +161,33 @@ while running:
             Population, fittest_creature = genetic(Population, NoOfBobs, fittest_creature, XWorld, YWorld, TurnsPerGen)
             for i in range(len(Population)):
                 Population[i].update_fitness(0)
+            print(len(Population))
             reset_food(all_coord_combos, L_food)
             generate_food(all_coord_combos, FoodPct, TallFoodPct, BushFoodPct, L_food)
             count = TurnsPerGen
             gens_left -= 1
-            if (gens_left < 0):
+            if (gens_left <= 0):
                 run_sim = False
+            attr1 = get_average_neck(Population)
+            last_plot1 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom,(Generations/10), graph_points, attr1, attr1_color, last_plot1)
+            #print("im neck", attr1)
+            attr2 = get_average_str(Population)
+            #last_plot2 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, Generations, gens_left, attr2, attr2_color, last_plot2)
+            attr3 = get_average_vision(Population)
+            #last_plot3 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, Generations, gens_left, attr3, attr3_color, last_plot3)
+            attr4 = get_average_stam(Population)
+            last_plot4 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, (Generations/10), graph_points, attr4, attr4_color, last_plot4)
+            #print("im stamina", attr4)
+            attr5 = get_average_speed(Population)
+            last_plot5 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, (Generations/10), graph_points, attr5, attr5_color, last_plot5)
+            #print("im speed", attr5)
+            graph_points -= 1
 
-        attr1 = get_average_neck(Population)
-        last_plot1 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, Generations, gens_left, attr1, attr1_color, last_plot1)
-        attr2 = get_average_str(Population)
-        #last_plot2 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, Generations, gens_left, attr2, attr2_color, last_plot2)
-        attr3 = get_average_vision(Population)
-        #last_plot3 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, Generations, gens_left, attr3, attr3_color, last_plot3)
-        attr4 = get_average_stam(Population)
-        last_plot4 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, Generations, gens_left, attr4, attr4_color, last_plot4)
-        attr5 = get_average_speed(Population)
-        last_plot5 = update_graph(pygame, Screen, Width, Height, BdrRight, BdrBottom, Generations, gens_left, attr5, attr5_color, last_plot5)
+        if graph_points <= 0:
+            last_plot1 = (-1, -1); last_plot2 = (-1, -1); last_plot3 = (-1, -1); last_plot4 = (-1, -1); last_plot5 = (-1, -1)
+            draw_axis(pygame, Screen, Width, Height, BdrRight, BdrBottom)
+            graph_points = Generations / 10
+
 
         # update the screen to match the new state of the world
         draw_grid(pygame, Screen, XWorld, YWorld, Width, Height, BdrLeft, BdrRight, BdrTop, BdrBottom, DrawGrid)
@@ -225,3 +241,5 @@ while running:
         StopButton.listen(event); StopButton.draw()
     # Handle the Pause Button
         PauseButton.listen(event); PauseButton.draw()
+        #handle the fittest button
+        ResetFittestButton.listen(event) ; ResetFittestButton.draw()
